979369c0f8fcf86e84e5d3199dca04492962e5ac
iliatapazukk <mozesinambient@gmail.com>
2021-04-13 09:53:11 +0300

    homework-4

--- main.js
+++ main.js
@@ -29,49 +29,77 @@ function createPlayer(playerObj){
   return $player;
 }
 
+const $arenas = document.querySelector('.arenas');
+
+function elHP(){
+  return document.querySelector('.player' + this.player + ' .life');
+}
+
+
+function getRandom(num) {
+  return Math.ceil(Math.random() * num);
+}
+
+function renderHP(){
+  this.elHP().style.width = this.hp + '%';
+}
+
+function changeHP(changeHP) {
+  this.hp -= changeHP;
+  if (this.hp <= 0) {
+    this.hp = 0
+    console.log('HP ZERO', this.hp)
+  }
+  return this.hp;
+}
+
+
 const player1 = {
   player: 1,
   name: 'sonya',
-  hp: 100
+  hp: 100,
+  elHP: elHP,
+  changeHP: changeHP,
+  renderHP: renderHP,
 }
 const player2 = {
   player: 2,
   name: 'subzero',
-  hp: 100
+  hp: 100,
+  elHP: elHP,
+  changeHP: changeHP,
+  renderHP: renderHP,
 }
 
-const $arenas = document.querySelector('.arenas');
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
 
-function playerWins(name){
+function showResultText(name){
  const $winsTitle = createElm('div', 'loseTitle');
- $winsTitle.innerText = `${name} wins`
+ if (name) {
+   $winsTitle.innerText = `${name} wins`
+ } else {
+   $winsTitle.innerText = `draw`
+ }
  return $winsTitle;
 }
 
 const $randomBtn = document.querySelector('.button');
 
-function changeHP(player) {
-  const $playerLife = document.querySelector(`.player${player.player} .life`)
-  player.hp -= Math.ceil(Math.random() * 40);
-  if (player.hp <= 0) {
-    player.hp = 0
-    $randomBtn.disabled = true
-  }
-  $playerLife.style.width = player.hp + '%';
-}
+$randomBtn.addEventListener('click', function() {
+  player1.changeHP(getRandom(20))
+  player2.changeHP(getRandom(20))
+  player1.renderHP();
+  player2.renderHP();
 
+  if (player1.hp === 0 || player2.hp === 0) { $randomBtn.disabled = true }
 
-$randomBtn.addEventListener('click', () => {
-  changeHP(player1)
-  changeHP(player2)
-
-  if (player1.hp <= 0) {
-    $arenas.appendChild(playerWins(player2.name));
-  } else if (player2.hp <= 0) {
-    $arenas.appendChild(playerWins(player1.name));
+  if (player1.hp === 0 && player1.hp < player2.hp) {
+    $arenas.appendChild(showResultText(player2.name));
+  } else if (player2.hp === 0 && player2.hp < player1.hp) {
+    $arenas.appendChild(showResultText(player1.name));
+  } else if (player1.hp === 0 && player2.hp === 0) {
+    $arenas.appendChild(showResultText());
   }
 
 })
-
